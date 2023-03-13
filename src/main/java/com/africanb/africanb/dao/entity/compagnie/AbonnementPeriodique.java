1d43884c678283d2c2518c8cfe7831c80c4be497
alzouma2021 <mahamadoualzouma12@gmail.com>
2023-03-13 12:34:04 +0000

    Gestion des modes d'abonnement

---
+++ src/main/java/com/africanb/africanb/dao/entity/compagnie/AbonnementPeriodique.java
@@ -0,0 +1,29 @@
+package com.africanb.africanb.dao.entity.compagnie;
+
+import com.africanb.africanb.dao.entity.offreVoyage.ValeurCaracteristiqueOffreVoyage;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * @Author Alzouma Moussa Mahamadou
+ */
+@Entity
+@Table(name="abonnementperiodique")
+@DiscriminatorValue("periodique")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AbonnementPeriodique extends ModeAbonnement implements Serializable {
+	@Column(nullable = false)
+	private long redevance ;
+	private long redevancePublicite ;
+}
---
+++ src/main/java/com/africanb/africanb/helper/dto/compagnie/ModeAbonnementDTO.java
@@ -0,0 +1,49 @@
+package com.africanb.africanb.helper.dto.compagnie;
+
+import com.africanb.africanb.helper.searchFunctions.SearchParam;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+/**
+ * @Author Alzouma Moussa Mahamadou
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ModeAbonnementDTO {
+
+
+	private Long id;
+	private String designation;
+	private String description;
+	private String dateDebutAbonnement ;
+	private String dateFinAbonnement ;
+	private String compagnieTransportRaisonSociale;
+	private String periodiciteAbonnementDesignation;
+
+	private Boolean  isDeleted;
+	private String updatedAt;
+	private Long  updatedBy;
+	private String createdAt;
+	private Long  createdBy;
+	private String deletedAt;
+	private Long  deletedBy;
+
+	private SearchParam<Boolean> isDeletedParam;
+	private SearchParam<String>   designationParam;
+	private SearchParam<String>   updatedAtParam;
+	private SearchParam<Long>     updatedByParam;
+	private SearchParam<String>   createdAtParam;
+	private SearchParam<Long>     createdByParam;
+
+	private String orderField;
+	private String orderDirection;
+
+}
